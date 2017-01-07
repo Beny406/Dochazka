6745ee01dc92943002277444c49e7e671e080475
Petr Beneš <beny406@seznam.cz>
2017-01-08 00:01:39 +0100

    about

--- src/main/java/cz/benes/database/dao/DBInitiator.java
+++
@@ -1,9 +0,0 @@
-package cz.benes.database.dao;
-
-/**
- * Created by Beny on 23.12.2016.
- */
-public interface DBInitiator {
-    void initiateDB();
-
-}
--- src/main/java/cz/benes/database/domain/RecordType.java
+++
@@ -1,5 +0,0 @@
-package cz.benes.database.domain;
-
-public enum RecordType {
-    IN, OUT, DOV, NEM, PAR
-}
